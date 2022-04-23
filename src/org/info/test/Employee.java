0d2fb8388ca69ef3f0804e03dc51efdcec604469
pradhap07 <m.n.pradhap@gmail.com>
2022-04-23 10:46:12 +0530

    JAVA day 1

---
+++ src/org/info/test/Employee.java
@@ -0,0 +1,21 @@
+package org.info.test;
+
+public class Employee {
+	private void empId() {
+		System.out.println("Employee id is 200");
+	}
+		private void empName() {
+	   System.out.println("Employee name is arun");
+		}
+	   private void empAge() {
+		   System.out.println("Employee age is 25");
+	   }
+	   public static void main(String[] args) {
+		Employee i = new Employee();
+		i.empId();
+		i.empName();
+		i.empAge();
+	   }
+	}
+    	   
+       
